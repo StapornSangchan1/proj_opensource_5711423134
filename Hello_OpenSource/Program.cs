6757e72bad376a442c0d22fb7325abcf2ec59e34
StapornSangchan <staporn.sang57@sskru.ac.th>
2016-09-09 14:13:09 +0700

    New Project

---
+++ Hello_OpenSource/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_OpenSource
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello My name is Staporn Sangchan 5711423134");
+
+            //keep the Console window open in debug mode.
+            Console.WriteLine("press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
